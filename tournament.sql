6652743a779b86d925ba752203a6e3f55a9d731a
azalea <mazhaorong@gmail.com>
2015-09-07 13:14:12 -0400

    Change SQL keywords to upper case

--- tournament.sql
+++ tournament.sql
@@ -4,49 +4,49 @@
 -- Make sure to back it up if the data are critical
 
 -- start from an empty database
-drop database if exists tournament;
-create database tournament;
+DROP DATABASE IF EXISTS tournament;
+CREATE DATABASE tournament;
 \c tournament;
 
 -- This table is necessary to support more than one tournament per database
 -- This table records the tournaments
-create table if not exists tournament (
-    id serial primary key,
+CREATE TABLE IF NOT EXISTS tournament (
+    id serial PRIMARY KEY,
     name text);
 
 -- This table records all the players
-create table if not exists player (
-    id serial primary key,
+CREATE TABLE IF NOT EXISTS player (
+    id serial PRIMARY KEY,
     name text);
 
 -- This table is necessary to support more than one tournament per database
 -- This table records which player has entered which tournament
-create table if not exists tournament_player (
-    tournament_id integer references tournament(id) on delete cascade,
-    player_id integer references player(id) on delete cascade,
-    primary key (tournament_id, player_id));
+CREATE TABLE IF NOT EXISTS tournament_player (
+    tournament_id integer REFERENCES tournament(id) ON DELETE CASCADE,
+    player_id integer REFERENCES player(id) ON DELETE CASCADE,
+    PRIMARY KEY (tournament_id, player_id));
 
 -- tournament_id column is needed to support
 -- more than one tournament per database
 -- is_draw column is used to denote whether the match result is a draw,
 -- to allow a draw (a tied game) as result.
-create table if not exists tournament_match_result (
-    id serial primary key,
-    tournament_id integer references tournament(id) on delete cascade,
-    winner_id integer references player(id) on delete cascade,
-    loser_id integer references player(id) on delete cascade,
-    is_draw boolean default false);
+CREATE TABLE IF NOT EXISTS tournament_match_result (
+    id serial PRIMARY KEY,
+    tournament_id integer REFERENCES tournament(id) ON DELETE CASCADE,
+    winner_id integer REFERENCES player(id) ON DELETE CASCADE,
+    loser_id integer REFERENCES player(id) ON DELETE CASCADE,
+    is_draw boolean DEFAULT false);
 
 -- This view is to get the current tournament conveniently
 -- when supporting more than one tournament per database
 -- The current tournament is the one with max id
-create or replace view current_tournament as
-    select * from tournament where id in (select max(id) from tournament);
+CREATE OR REPLACE VIEW current_tournament AS
+    SELECT * FROM tournament WHERE id IN (SELECT max(id) FROM tournament);
 
 -- This view is to get the players in the current tournament conveniently
 -- when supporting more than one tournament per database
-create or replace view current_player as
-    select * from player where id in
-        (select player_id from tournament_player where tournament_id in
-            (select max(id) from tournament));
+CREATE OR REPLACE VIEW current_player AS
+    SELECT * FROM player WHERE id IN
+        (SELECT player_id FROM tournament_player WHERE tournament_id IN
+            (SELECT max(id) FROM tournament));
 
